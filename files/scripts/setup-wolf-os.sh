570f7f21bb2f076437c44142f6595709a7bad8fa
Jonathon Poppleton <jonathonpoppleton@posteo.net>
2026-07-07 15:44:59 +1000

    specified vpn group id

--- files/scripts/setup-wolf-os.sh
+++ files/scripts/setup-wolf-os.sh
@@ -7,15 +7,23 @@ groupadd -r docker || true
 groupadd -r libvirt-qemu || true
 groupadd -r virtnetwork || true
 
-
-# --- 7. AUTOMATED CLEANUP ---
+# --- 2. AUTOMATED CLEANUP ---
 echo "⚙️ Setting up First-Boot cleanup service..."
 
 # Ensure clean up script is executable
 chmod +x /usr/libexec/wolf-os-firstboot.sh
 
-# --- 8. FINALISE ---
-systemctl enable libvirtd.service virtlogd.service virtnetworkd.service virtstoraged.service virtnodedevd.socket sshd.service docker.service wolf-os-cleanup.service apps-tmpfiles.service
+# --- 3. FINALISE ---
+systemctl enable \
+    libvirtd.service \
+    virtlogd.service \
+    virtnetworkd.service \
+    virtstoraged.service \
+    virtnodedevd.socket \
+    sshd.service \
+    docker.service \
+    wolf-os-cleanup.service \
+    app-tmpfiles.service
 
 echo "✅ Wolf-OS Custom Assembly Complete! Ready for Deployment."
 
